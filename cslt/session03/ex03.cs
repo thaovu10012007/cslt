85e36148bea211c6a362a2b7866a6ac55c465741
VuThanhThao <thaovu10012007@gmail.com>
2026-08-21 02:25:18 -0700

    capnhatex03

---
+++ cslt/session03/ex03.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cslt.session03
+{
+    internal class ex_2
+    {
+        static void Bai_1() 
+            static void Main(string[] args)
+            {
+                Console.Write("Inpur celsius: ");
+                if (double.TryParse(Console.ReadLine(), out double celsius))
+                {
+                    double kelvin = celsius + 273;
+                    double fahrenheit = celsius * 18 / 10 + 32;
+                    Console.WriteLine($"kelvin = {kelvin}");
+                    Console.WriteLine($"fahrenheit = {fahrenheit}");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect, please try again");
+                }
+        
+            }
+        }
+    }
+
